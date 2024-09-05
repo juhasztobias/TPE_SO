a85d426ab7daaff2dcc31384db437c970fa738fb
nnicokim <nkim@itba.edu.ar>
2024-09-05 20:25:34 -0300

    changes

--- main.c
+++ main.c
@@ -14,63 +14,85 @@
 #define SLAVES 5
 #define SHM_NAME "/md5_shared_memory"
 #define SHM_SIZE 4096
+#define TWO 2
 
-int main(int argc, char * argv[]) {
-    pid_t pid;
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
+int main(int argc, char *argv[])
+{
+    pid_t pid[SLAVES];
+    int file_pipes[SLAVES][TWO];  // Pipes main -> slave
+    int hash_pipes[SLAVES][TWO];  // Pipes slave -> main
 
-    system("whereis md5sum");
     for (int i = 0; i < SLAVES; i++)
     {
-        pid = fork();
-        if (pid == -1)
-        {
+        if (pipe(file_pipes[i]) == -1) {
+            perror("file_pipe");
+            exit(EXIT_FAILURE);
+        }
+        if (pipe(hash_pipes[i]) == -1) {
+            perror("hash_pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < SLAVES; i++)
+    {
+        pid[i] = fork();
+        if (pid[i] == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
         }
-        if (pid == 0)
-        {
-            close(0);
-            dup(pipefd[0]);
-            close(pipefd[0]);
-            close(pipefd[1]);
+        if (pid[i] == 0) { // Child (slave)
+            close(file_pipes[i][WRITE_FD]);  // Close write-end of file pipe
+            // close(hash_pipes[i][READ_FD]);   // Close read-end of hash pipe
 
-            char * slave_o = "../src/bin/slave.o";
-            char * slave_argv[] = {slave_o, NULL};
+            // Redirect pipes to stdin and stdout for the child
+            dup2(file_pipes[i][READ_FD], STDIN_FILENO);   // Redirect file pipe read-end to stdin
+            // dup2(hash_pipes[i][WRITE_FD], STDOUT_FILENO); // Redirect hash pipe write-end to stdout
+
+            // Close the originals after dup
+            close(file_pipes[i][READ_FD]);
+            // close(hash_pipes[i][WRITE_FD]);
+
+            char *slave_o = "../src/bin/slave.o";
+            char *slave_argv[] = {slave_o, NULL};
             execve(slave_o, slave_argv, NULL);
 
             perror("execve");
             exit(EXIT_FAILURE);
         }
     }
-    
-    fsync(1);
 
-    struct pollfd pfd;
-    pfd.fd = pipefd[READ_FD];
-    pfd.events = POLLIN;
-    for(int i = 1; i < argc; i++) {
-        int ret = poll(&pfd, 1, 1);
+    fsync(1);
+    
+    // Armo las estrcturas
+    struct pollfd pfd[SLAVES];
+    for(int i = 0; i < SLAVES; i++){
+        pfd[i].fd = file_pipes[i][WRITE_FD]; //REVISAR
+        pfd[i].events = POLLIN;
+    }
+
+
+    // // Máximo de 5 archivos por slave.
+    // // Porque son 2 slaves y son 10 archivos
+    int slave = 0;
+    for(int i = 1; i < argc; i++, slave++){
+        slave = slave % SLAVES;
+        int ret = poll(&(pfd[slave]), 1, 1);
         if (ret == -1) {
             perror("poll");
             exit(EXIT_FAILURE);
         }
-        if (ret == 0) {
-            write(pipefd[WRITE_FD], argv[i], strlen(argv[i]) + 1); 
+        if(ret == 0){
+            printf("slave: %d\n", slave);
+            write(file_pipes[slave][WRITE_FD], argv[i], strlen(argv[i]) + 1);
             continue;
         }
-
         i--;
     }
 
     // main.c debe recibir los md5 de los slave.c, para imprimir por pantalla y agregarlo a la shm por orden de llegada
 
-
-    // creamos la shm 
+    // creamos la shm
 
     /*
     int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
@@ -95,12 +117,21 @@ int main(int argc, char * argv[]) {
     close(shm_fd);
     shm_unlink(SHM_NAME);
     */
-
-    close(pipefd[READ_FD]);
-    close(pipefd[WRITE_FD]);
+   
+    for(int i = 0; i < SLAVES; i++) {
+        close(file_pipes[i][READ_FD]);
+        close(hash_pipes[i][READ_FD]);
+        close(file_pipes[i][WRITE_FD]);
+        close(hash_pipes[i][WRITE_FD]);
+    }
+    printf("aca llega\n");
 
     int status = 0;
-    while((pid = wait(&status)) > 0);
+    for (int i = 0; i < SLAVES; i++) {
+        while ((pid[i] = wait(&status)) > 0);
+    }
 
+    printf("aca llega\n");
+    
     return 0;
 }
--- src/slave.c
+++ src/slave.c
@@ -2,6 +2,7 @@
 // #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <semaphore.h>
 
 #define MD5_COMMAND "/sbin/md5 "            // PATH AL MD5SUM en MacOS M3 (Nico)
 // #define MD5_COMMAND "/sbin/md5sum"         // PATH AL MD5SUM en MacOS M1
@@ -13,14 +14,15 @@
 #define BUFFER_SIZE 1024
 #define COMMAND_SIZE 256
 
+
 int main(int argc, char *argv[]){
     char file[BUFFER_SIZE];
     pid_t pid = getpid();
     fsync(0);
     ssize_t buffer_read;
-
     while((buffer_read = read(STDIN_FILENO, file, BUFFER_SIZE * sizeof(char))) > 0) {
         char md5sum_command[COMMAND_SIZE];
+        fprintf(stdout, "Estoy leyendo un file %s\n", file);
         sprintf(md5sum_command, "%s \'%s\'", MD5_COMMAND, file);
 
         FILE * fp = popen(md5sum_command, "r");
@@ -31,17 +33,34 @@ int main(int argc, char *argv[]){
 
     // estamos imprimiendo en pantalla/terminal directamente desde el slave
     // tenemos que pasarle por los pipes al main el dato formateado para que el main imprima por pantalla e inserte en la shm 
-    
+
+        /*if (sem_init(&semaphore, 0, 2) == -1) {
+           perror("sem_init");
+           exit(EXIT_FAILURE); 
+        }*/
 
         char md5Buffer[BUFFER_SIZE];
+
+        // cuando lee dos files, el semaphore tiene que bloquear
+        
         while(fgets(md5Buffer, sizeof(md5Buffer), fp) != NULL) {
             char md5[BUFFER_SIZE];
             sscanf(md5Buffer, "%s ", md5);
-            fprintf(stdout, "File: %s - MD5: %s - PID: %d\n", file, md5, pid);
+            fprintf(stdout, "%s", md5Buffer);
+            // fprintf(stdout, "File: %s - MD5: %s - PID: %d\n", file, md5, pid);
         }      
 
         // fprintf(stdout, "%d\n", pid);
+        fprintf(stdout, "%s", "Terminé");
         pclose(fp);
     }
     return 0;
 }
+
+
+/*sem_t semaphore;
+
+sem_init(&semaphore, 0, 1);  // Initialize semaphore with a value of 1
+sem_wait(&semaphore);        // Decrease (wait)
+sem_post(&semaphore);        // Increase (signal)
+sem_destroy(&semaphore);     // Clean up*/
