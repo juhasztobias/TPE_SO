7b64449a24ca9511c16591b2d2f0a0a4b9b03fa5
Tobias Juhasz <108370047+juhasztobias@users.noreply.github.com>
2024-09-05 21:58:06 -0300

    Todos los archivos leidos

--- main.c
+++ main.c
@@ -19,16 +19,18 @@
 int main(int argc, char *argv[])
 {
     pid_t pid[SLAVES];
-    int file_pipes[SLAVES][TWO];  // Pipes main -> slave
-    int hash_pipes[SLAVES][TWO];  // Pipes slave -> main
+    int file_pipes[SLAVES][TWO]; // Pipes main -> slave
+    int hash_pipes[SLAVES][TWO]; // Pipes slave -> main
 
     for (int i = 0; i < SLAVES; i++)
     {
-        if (pipe(file_pipes[i]) == -1) {
+        if (pipe(file_pipes[i]) == -1)
+        {
             perror("file_pipe");
             exit(EXIT_FAILURE);
         }
-        if (pipe(hash_pipes[i]) == -1) {
+        if (pipe(hash_pipes[i]) == -1)
+        {
             perror("hash_pipe");
             exit(EXIT_FAILURE);
         }
@@ -37,21 +39,21 @@ int main(int argc, char *argv[])
     for (int i = 0; i < SLAVES; i++)
     {
         pid[i] = fork();
-        if (pid[i] == -1) {
+        if (pid[i] == -1)
+        {
             perror("fork");
             exit(EXIT_FAILURE);
         }
-        if (pid[i] == 0) { // Child (slave)
-            close(file_pipes[i][WRITE_FD]);  // Close write-end of file pipe
-            // close(hash_pipes[i][READ_FD]);   // Close read-end of hash pipe
+        if (pid[i] == 0)
+        { // Child (slave)
+            // close(STDIN_FILENO);  // Close write-end of file pipe
 
             // Redirect pipes to stdin and stdout for the child
-            dup2(file_pipes[i][READ_FD], STDIN_FILENO);   // Redirect file pipe read-end to stdin
+            dup2(file_pipes[i][READ_FD], STDIN_FILENO); // Redirect file pipe read-end to stdin
             // dup2(hash_pipes[i][WRITE_FD], STDOUT_FILENO); // Redirect hash pipe write-end to stdout
 
             // Close the originals after dup
             close(file_pipes[i][READ_FD]);
-            // close(hash_pipes[i][WRITE_FD]);
 
             char *slave_o = "../src/bin/slave.o";
             char *slave_argv[] = {slave_o, NULL};
@@ -62,76 +64,43 @@ int main(int argc, char *argv[])
         }
     }
 
-    fsync(1);
-    
     // Armo las estrcturas
     struct pollfd pfd[SLAVES];
-    for(int i = 0; i < SLAVES; i++){
-        pfd[i].fd = file_pipes[i][WRITE_FD]; //REVISAR
+    for (int i = 0; i < SLAVES; i++)
+    {
+        pfd[i].fd = file_pipes[i][READ_FD];
         pfd[i].events = POLLIN;
     }
 
-
-    // // Máximo de 5 archivos por slave.
-    // // Porque son 2 slaves y son 10 archivos
-    int slave = 0;
-    for(int i = 1; i < argc; i++, slave++){
+    int slave = 0, i = 1;
+    while (i < argc)
+    {
+        slave++;
         slave = slave % SLAVES;
         int ret = poll(&(pfd[slave]), 1, 1);
-        if (ret == -1) {
+        if (ret == -1)
+        {
             perror("poll");
             exit(EXIT_FAILURE);
         }
-        if(ret == 0){
-            printf("slave: %d\n", slave);
+        if (ret == 0)
+        {
             write(file_pipes[slave][WRITE_FD], argv[i], strlen(argv[i]) + 1);
+            i++;
             continue;
         }
-        i--;
     }
 
-    // main.c debe recibir los md5 de los slave.c, para imprimir por pantalla y agregarlo a la shm por orden de llegada
-
-    // creamos la shm
-
-    /*
-    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
-    if (shm_fd == -1) {
-        perror("shm_open");
-        exit(EXIT_FAILURE);
-    }
-
-    if (ftruncate(shm_fd, SHM_SIZE) == -1) {
-        perror("ftruncate");
-        exit(EXIT_FAILURE);
-    }
-
-    void *shm_ptr = mmap(0, SHM_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0); // PROT_WRITE permite la escritura en la shm
-    if (shm_ptr == MAP_FAILED) {
-        perror("mmap");
-        exit(EXIT_FAILURE);
-    }
-
-    // elimino y cierro todo lo de la shm
-    munmap(shm_ptr, SHM_SIZE);
-    close(shm_fd);
-    shm_unlink(SHM_NAME);
-    */
-   
-    for(int i = 0; i < SLAVES; i++) {
+    for (int i = 0; i < SLAVES; i++)
+    {
         close(file_pipes[i][READ_FD]);
         close(hash_pipes[i][READ_FD]);
         close(file_pipes[i][WRITE_FD]);
         close(hash_pipes[i][WRITE_FD]);
     }
-    printf("aca llega\n");
 
-    int status = 0;
-    for (int i = 0; i < SLAVES; i++) {
-        while ((pid[i] = wait(&status)) > 0);
-    }
-
-    printf("aca llega\n");
-    
+    // int status = 0;
+    while ((waitpid(-1, NULL, 0)) > 0)
+        printf("Esperando\n");
     return 0;
 }
--- src/slave.c
+++ src/slave.c
@@ -3,60 +3,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <poll.h>
 
-#define MD5_COMMAND "/sbin/md5 "            // PATH AL MD5SUM en MacOS M3 (Nico)
-// #define MD5_COMMAND "/sbin/md5sum"         // PATH AL MD5SUM en MacOS M1
+// #define MD5_COMMAND "/sbin/md5 "            // PATH AL MD5SUM en MacOS M3 (Nico)
+#define MD5_COMMAND "/sbin/md5sum" // PATH AL MD5SUM en MacOS M1
 // #define MD5_COMMAND "/usr/bin/md5sum "   // PATH AL MD5SUM en Docker Linux
 #define READ_FD 0
 #define WRITE_FD 1
 #define WAIT_DEFAULT 0
 #define LOOP 1
 #define BUFFER_SIZE 1024
-#define COMMAND_SIZE 256
+#define COMMAND_SIZE 2048
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     char file[BUFFER_SIZE];
     pid_t pid = getpid();
-    fsync(0);
+    fsync(STDIN_FILENO);
     ssize_t buffer_read;
-    while((buffer_read = read(STDIN_FILENO, file, BUFFER_SIZE * sizeof(char))) > 0) {
+
+    while ((buffer_read = read(STDIN_FILENO, file, BUFFER_SIZE * sizeof(char))) >= 0)
+    {
+
         char md5sum_command[COMMAND_SIZE];
-        fprintf(stdout, "Estoy leyendo un file %s\n", file);
         sprintf(md5sum_command, "%s \'%s\'", MD5_COMMAND, file);
 
-        FILE * fp = popen(md5sum_command, "r");
-        if (fp == NULL) {
+        FILE *fp = popen(md5sum_command, "r");
+        if (fp == NULL)
+        {
             perror("popen");
             exit(EXIT_FAILURE);
         }
 
-    // estamos imprimiendo en pantalla/terminal directamente desde el slave
-    // tenemos que pasarle por los pipes al main el dato formateado para que el main imprima por pantalla e inserte en la shm 
-
-        /*if (sem_init(&semaphore, 0, 2) == -1) {
-           perror("sem_init");
-           exit(EXIT_FAILURE); 
-        }*/
-
         char md5Buffer[BUFFER_SIZE];
 
-        // cuando lee dos files, el semaphore tiene que bloquear
-        
-        while(fgets(md5Buffer, sizeof(md5Buffer), fp) != NULL) {
+        while (fgets(md5Buffer, sizeof(md5Buffer), fp) != NULL)
+        {
             char md5[BUFFER_SIZE];
             sscanf(md5Buffer, "%s ", md5);
-            fprintf(stdout, "%s", md5Buffer);
-            // fprintf(stdout, "File: %s - MD5: %s - PID: %d\n", file, md5, pid);
-        }      
+            fprintf(stdout, "File: %s - MD5: %s - PID: %d\n", file, md5, pid);
+        }
 
-        // fprintf(stdout, "%d\n", pid);
-        fprintf(stdout, "%s", "Terminé");
         pclose(fp);
     }
+
     return 0;
 }
-
 
 /*sem_t semaphore;
 
